e57120a990c4d2de9de9ccc6964e4344f62c64a6
IvannaKozak <ivanka.kozak3@gmail.com>
2022-08-25 00:09:06 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AudioSchool',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-    //  home: ,
+      //  home: ,
     );
   }
 }
