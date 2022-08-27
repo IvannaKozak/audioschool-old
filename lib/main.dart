0cff5fd92c26a29f07ef5091681631299d2a5fda
IvannaKozak <ivanka.kozak3@gmail.com>
2022-08-27 13:47:07 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioschool/screens/main_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 import 'package:audioschool/screens/loading_screen.dart';
 
 void main() {
@@ -39,3 +38,4 @@ class MyApp extends StatelessWidget {
 //           //useLoader: SpinKitCircle(),
 //           loaderColor: Colors.white,
 //         )
+
