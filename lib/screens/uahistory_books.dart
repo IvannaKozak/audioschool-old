98f929c1c13ad7eceb2fb974b8d2aa92809e8cdd
IvannaKozak <ivanka.kozak3@gmail.com>
2022-08-28 20:18:12 +0300

    scroll commit

--- lib/screens/uahistory_books.dart
+++ lib/screens/uahistory_books.dart
@@ -25,140 +25,173 @@ class _BooksScreenState extends State<BooksScreen> {
         title: Text('AudioSchool'),
         backgroundColor: Color(0xFF14284B),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: <Widget>[
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 12.0),
-                      backgroundColor:
-                          _hasBeenPressed[0] ? kInactiveColor : kActiveColor,
-                      //side: BorderSide(color: Colors.white, width: 5),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          side: BorderSide(width: 1.0, color: Colors.white)),
-                      fixedSize: Size.fromWidth(110),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20.0,
                     ),
-                    onPressed: () => {
-                      setState(() {
-                        _hasBeenPressed[0] = !_hasBeenPressed[0];
-                      })
-                    },
-                    child: Text(
-                      '9 клас',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        backgroundColor:
+                            _hasBeenPressed[0] ? kInactiveColor : kActiveColor,
+                        //side: BorderSide(color: Colors.white, width: 5),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            side: BorderSide(width: 1.0, color: Colors.white)),
+                        fixedSize: Size.fromWidth(110),
+                      ),
+                      onPressed: () => {
+                        setState(() {
+                          _hasBeenPressed[0] = !_hasBeenPressed[0];
+                        })
+                      },
+                      child: Text(
+                        '9 клас',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 12.0),
-                      // backgroundColor: Color(0xFFD9D9D9),
-                      backgroundColor:
-                          _hasBeenPressed[1] ? kInactiveColor : kActiveColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          side: BorderSide(color: Color(0xFFD9D9D9))),
-                      fixedSize: Size.fromWidth(110),
+                    SizedBox(
+                      width: 20.0,
                     ),
-                    onPressed: () => {
-                      setState(() {
-                        _hasBeenPressed[1] = !_hasBeenPressed[1];
-                      })
-                    },
-                    child: Text(
-                      '10 клас',
-                      style: TextStyle(
-                          color: Color(0xFF726565),
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        // backgroundColor: Color(0xFFD9D9D9),
+                        backgroundColor:
+                            _hasBeenPressed[1] ? kInactiveColor : kActiveColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            side: BorderSide(color: Color(0xFFD9D9D9))),
+                        fixedSize: Size.fromWidth(110),
+                      ),
+                      onPressed: () => {
+                        setState(() {
+                          _hasBeenPressed[1] = !_hasBeenPressed[1];
+                        })
+                      },
+                      child: Text(
+                        '10 клас',
+                        style: TextStyle(
+                            color: Color(0xFF726565),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 12.0),
-                      backgroundColor:
-                          _hasBeenPressed[2] ? kInactiveColor : kActiveColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          side: BorderSide(width: 0.0, color: Colors.white)),
-                      fixedSize: Size.fromWidth(110),
+                    SizedBox(
+                      width: 20.0,
                     ),
-                    onPressed: () => {
-                      setState(() {
-                        _hasBeenPressed[2] = !_hasBeenPressed[2];
-                      })
-                    },
-                    child: Text(
-                      '11 клас',
-                      style: TextStyle(
-                          color: Color(0xFF726565),
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        backgroundColor:
+                            _hasBeenPressed[2] ? kInactiveColor : kActiveColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            side: BorderSide(width: 0.0, color: Colors.white)),
+                        fixedSize: Size.fromWidth(110),
+                      ),
+                      onPressed: () => {
+                        setState(() {
+                          _hasBeenPressed[2] = !_hasBeenPressed[2];
+                        })
+                      },
+                      child: Text(
+                        '11 клас',
+                        style: TextStyle(
+                            color: Color(0xFF726565),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 12.0),
-                      backgroundColor:
-                          _hasBeenPressed[3] ? kInactiveColor : kActiveColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          side: BorderSide(width: 0.0, color: Colors.white)),
-                      fixedSize: Size.fromWidth(110),
+                    SizedBox(
+                      width: 20.0,
                     ),
-                    onPressed: () => {
-                      setState(() {
-                        _hasBeenPressed[3] = !_hasBeenPressed[3];
-                      })
-                    },
-                    child: Text(
-                      '12 клас',
-                      style: TextStyle(
-                          color: Color(0xFF726565),
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        backgroundColor:
+                            _hasBeenPressed[3] ? kInactiveColor : kActiveColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            side: BorderSide(width: 0.0, color: Colors.white)),
+                        fixedSize: Size.fromWidth(110),
+                      ),
+                      onPressed: () => {
+                        setState(() {
+                          _hasBeenPressed[3] = !_hasBeenPressed[3];
+                        })
+                      },
+                      child: Text(
+                        '12 клас',
+                        style: TextStyle(
+                            color: Color(0xFF726565),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: BookCard(
-                      bookImage:
-                          Image(image: AssetImage('images/history_1.jpg')),
-                      bookTitle: 'Історія України 9 клас',
-                      bookAuthor: 'О.В. Гісем, О.О. Мартинюк',
+            SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: BookCard(
+                        bookImage:
+                            Image(image: AssetImage('images/history_1.jpg')),
+                        bookTitle: 'Історія України 9 клас',
+                        bookAuthor: 'О.В. Гісем, О.О. Мартинюк',
+                      ),
                     ),
-                  )
-                ],
-              )),
-        ],
+                    SizedBox(height: 20.0,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: BookCard(
+                        bookImage:
+                            Image(image: AssetImage('images/history_1.jpg')),
+                        bookTitle: 'Біологія 10 клас',
+                        bookAuthor: 'О.В. Гісем, О.О. Мартинюк',
+                      ),
+                    ),
+                    SizedBox(height: 20.0,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: BookCard(
+                        bookImage:
+                            Image(image: AssetImage('images/history_1.jpg')),
+                        bookTitle: 'Історія України 9 клас',
+                        bookAuthor: 'О.В. Гісем, О.О. Мартинюк',
+                      ),
+                    ),
+                    SizedBox(height: 20.0,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: BookCard(
+                        bookImage:
+                            Image(image: AssetImage('images/history_1.jpg')),
+                        bookTitle: 'Алгебра 11 клас',
+                        bookAuthor: 'О.В. Гісем, О.О. Мартинюк',
+                      ),
+                    )
+                  ],
+                ),),
+          ],
+        ),
       ),
     );
   }
